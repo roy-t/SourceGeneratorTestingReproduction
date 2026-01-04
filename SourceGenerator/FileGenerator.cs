d8f333f364a6cb7ebdfb9385894ffb5f7b3b975d
Roy Triesscheijn <roy-t@hotmail.com>
2026-01-04 19:58:01 +0100

    init

---
+++ SourceGenerator/FileGenerator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace SourceGenerator;
+
+public class FileGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(static postInitializationContext =>
+        {
+            postInitializationContext.AddEmbeddedAttributeDefinition();
+            var builder = new StringBuilder();
+            var code = """
+            using System;
+
+            namespace SourceGenerator
+            {
+                public class GeneratedClass
+                {
+
+                }
+            }
+            """;
+
+            var source = SourceText.From(code, Encoding.UTF8);
+            postInitializationContext.AddSource("GeneratedClass.g.cs", source);
+        });
+    }
+}
